abdae295efcab7f27714f2239e3234ebe74d9348
Ilya <sir.trubin000@yandex.ru>
2022-06-16 22:25:04 +0300

    Написал коммент, ура!

--- generalModule/src/main/java/org/mainModule/common/CommonUtils.java
+++ generalModule/src/main/java/org/mainModule/common/CommonUtils.java
@@ -10,7 +10,7 @@ import java.util.Date;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
-
+//This comment
 public class CommonUtils {
     public static void waiter(int timeoutInMS){
         try {
