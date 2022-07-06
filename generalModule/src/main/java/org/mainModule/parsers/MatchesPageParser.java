80381d2a04c4ee28d3a3ec9b96a6cd21a5a6dbb1
Ilya <sir.trubin000@yandex.ru>
2022-07-06 21:18:37 +0300

    Распарсл игроков в matches.

--- generalModule/src/main/java/org/mainModule/parsers/MatchesPageParser.java
+++ generalModule/src/main/java/org/mainModule/parsers/MatchesPageParser.java
@@ -1,6 +1,5 @@
 package org.mainModule.parsers;
 
-import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
 import org.mainModule.common.CommonUtils;
@@ -8,7 +7,6 @@ import org.mainModule.common.CommonUtils;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
-import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 
 public class MatchesPageParser {
@@ -20,27 +18,38 @@ public class MatchesPageParser {
         Document doc = CommonUtils.reliableConnectAndGetDocument("https://www.hltv.org/matches");
         if (doc != null) {
             Elements elementsWithHrefs = doc.body().getElementsByClass("upcomingMatch");
-            listOfLinks = elementsWithHrefs.stream().map(element -> {
-                //элемент в childNodes содержит две или одну ноду. Две если известны соперники и есть кнопка ставок. Одна в обратном случае.
-                //нода с ссылкой на матч всегда стоит первой, но для проверки нужно смотреть на класс, который лежит в атрибутах
-                //этот класс получается "match a-reset". У ставок класс "matchAnalytics"
-                //В ноде с есть два атрибута в виде мапы. По ключу href находится чистая ссылка на матч
-                //ссылка такого образца /matches/2356525/eternal-fire-vs-saw-esl-pro-league-season-16-conference-play-in
-                if (element.childNodes().get(0).attributes().get("class").equals("match a-reset") && element.childNodes().size() == 2) {
-                    return "https://www.hltv.org" + element.childNodes().get(0).attributes().get("href");
-                } else return null;
-            }).filter(Objects::nonNull).collect(Collectors.toList());
+            listOfLinks.addAll(getMatchesHrefs(elementsWithHrefs));
+            elementsWithHrefs = doc.body().getElementsByClass("liveMatch");
+            listOfLinks.addAll(getMatchesHrefs(elementsWithHrefs));
+            //listOfLinks.add("https://www.hltv.org/matches/2357236/onetap-vs-norway-european-championship-2022"); // Назову это "проблемный матч". Он чисто для теста
         }
         System.out.println("Запрос будущих матчей: " + (System.currentTimeMillis() - now));
         System.out.println("Всего возможных матчей: " + listOfLinks.size());
-        parseAllPlayers(listOfLinks.subList(0, 1));
+        //parseAllPlayers(listOfLinks.subList(0, 1));
+        parseAllPlayers(listOfLinks);
     }
 
     private void parseAllPlayers(List<String> listOfLinks) {
-        AtomicInteger iterator = new AtomicInteger();
-        listOfLinks.forEach(link -> {
-            iterator.getAndIncrement();
-            matchPlayer.parseMatch(link);
-        });
+        listOfLinks.forEach(matchPlayer::parseMatch);
+    }
+
+    // Создал этот метод потому что получается, что нужно делать 2 одинаковых действия, в начале для лайвов, а потом для других матчей.
+    // Не знаю как лучше выводить список. Можно сделать это как сделано сейчас, чтоб метод возвращал лист, либо сделать
+    // метод войдом, но он сразу же будет запихивать ссылки в listOfLinks.
+    private List<String> getMatchesHrefs(Elements elementsWithHrefs) {
+        //элемент в childNodes содержит две или одну ноду. Две если известны соперники и есть кнопка ставок. Одна в обратном случае.
+        //нода с ссылкой на матч всегда стоит первой, но для проверки нужно смотреть на класс, который лежит в атрибутах
+        //этот класс получается "match a-reset". У ставок класс "matchAnalytics"
+        //В ноде с есть два атрибута в виде мапы. По ключу href находится чистая ссылка на матч
+        //ссылка такого образца /matches/2356525/eternal-fire-vs-saw-esl-pro-league-season-16-conference-play-in
+        List<String> links = elementsWithHrefs.stream().map(element -> {
+            // Здесь двойная проверка на матч, доступный для ставок. Во-первых, у него должно быть две ноды. Во-вторых,
+            // класс второй ноды должен быть "matchAnalytics".
+            if (element.childNodes().get(0).attributes().get("class").equals("match a-reset") && element.childNodes().size() == 2 &&
+                    element.childNodes().get(1).attributes().get("class").equals("matchAnalytics")) {
+                return "https://www.hltv.org" + element.childNodes().get(0).attributes().get("href");
+            } else return null;
+        }).filter(Objects::nonNull).collect(Collectors.toList());
+        return links;
     }
 }
